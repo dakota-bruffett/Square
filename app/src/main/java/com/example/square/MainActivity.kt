2c5ff7648026a3dd081f5c568a95046c823ac89b
nq2623up <DakotaBruffett@gmail.com>
2023-02-15 14:23:28 -0600

    puah

--- app/src/main/java/com/example/square/MainActivity.kt
+++ app/src/main/java/com/example/square/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.square
 
+import android.app.Activity
+import android.app.Instrumentation.ActivityResult
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+const val EXTRA_SQUARE_SIZE = "com.example.Square.SQUARE_SIZE"
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var Square_button: Button
+    private lateinit var Instructions: TextView
+    private lateinit var Show_Square_button:LinearLayout
+    private val SquareResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            Result -> handleSquareResult(result)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Square_button = findViewById(R.id.Square_button)
+        Instructions = findViewById(R.id.Instrutions)
+        Show_Square_button = findViewById(R.id.Show_square_button)
+        val intialprogress = Square_button
+        updateSquare(intialprogress)
+
+        Show_Square_button.setOnClickListener{
+            showSquare()
+
+        }
+
+
+    }
+    private fun showSquare(){
+        val showSquareIntent = intent(this,Activity::class.java)
+        showSquareIntent.putExta(EXTRA_SQUARE_SIZE,Square_button.progress)
+       // startActivity(showSquareIntent)
+        SquareResult.launch(showSquareIntent)
+    }
+    private fun handleSquareResult(result:ActivityResult){
+
     }
 }
--- app/src/main/java/com/example/square/Tap.kt
+++ app/src/main/java/com/example/square/Tap.kt
@@ -1,11 +1,42 @@
 package com.example.square
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.text.method.Touch
+import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+const val EXTRA_TAPPED_INSIDE_SQUARE = "com.example.Square.SQUARE_SIZE"
 class Tap : AppCompatActivity() {
+    private lateinit var poke:ImageView
+    private lateinit var Touch:TextView
+    private lateinit var Contain:View
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_tap)
+        poke = findViewById(R.id.Poke)
+        Contain = findViewById(R.id.Contain)
+
+        var pokeSize = intent.getIntExtra(EXTRA_SQUARE_SIZE, 100)
+        if (pokeSize == 0){
+            pokeSize== 1
+        }
+        poke.layoutParams.height = pokeSize
+        poke.layoutParams.width = pokeSize
+
+        poke.setOnClickListener {
+            pokeTouched(true)
+
+        }
+        Contain.setOnClickListener {
+            pokeTouched(false)
+        }
+
     }
+
 }
